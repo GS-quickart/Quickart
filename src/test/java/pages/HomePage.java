3046d124158e42b8aef155a04fa1e7c52549aece
Admin <Admin@DESKTOP-B7VKUIG>
2026-05-11 16:24:51 +0530

    updated file

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -29,13 +29,11 @@ public class HomePage
     // Reusable Method
     public void addRandomProductFromCategory(String categoryName) throws InterruptedException
     {
-    	Thread.sleep(5000);
+    	Thread.sleep(2500);
 		
 		WebElement Categories = driver.findElement(By.xpath("//h5[text()='Categories']"));
     	JavascriptExecutor js = (JavascriptExecutor) driver;
-    	js.executeScript("arguments[0].scrollIntoView(true);", Categories);
-    	Thread.sleep(2000);
-    	js.executeScript("window.scrollBy(0,-300);");
+    	js.executeScript("arguments[0].scrollIntoView({block:'center'});",Categories);
 		Thread.sleep(1000);
     	
 	    // Open category
@@ -72,28 +70,33 @@ public class HomePage
 	    Thread.sleep(2000);
 	    
 	    //Get Coordinate of selected product
+	    js.executeScript("arguments[0].scrollIntoView({block:'center'});",selectedProduct);
+	    Thread.sleep(2000);
+/*
 	    int Product_position = selectedProduct.getLocation().getY();
 	    System.out.println("Y Coordinate of selected product : " + Product_position);
 	    js.executeScript("window.scrollBy(0, arguments[0]);", Product_position);
 	    Thread.sleep(2000);
 	    js.executeScript("window.scrollBy(0,-400)");
 	    Thread.sleep(2000);
+  */
   
-	    // Click on Add(+) button of selected product
-	    selectedProduct.findElement(By.xpath("(//button[contains(text(),'+')])[" + randomIndex + "]")).click();
-	    //System.out.println("selectedProduct- " + selectedProduct);
 	   
 	    //Handle Variant product
-	    String Variant = driver.findElement(By.xpath("(//div[@class='product']//span[contains(.,'options')])[" + randomIndex + "]")).getText();
-	    System.out.println(Variant);
-	    if(Variant.equalsIgnoreCase("Done"))
+	    //String Variant = driver.findElement(By.xpath("(//div[@class='product'])[" + randomIndex + "]//span[contains(.,'options')]")).getText();
+	    String Variant = driver.findElement(By.xpath("(//div[@class='product'])[" + randomIndex + "]")).getText();
+
+	    System.out.println("Variant : "+Variant);
+	    if(Variant.contains("options"))
 	    {
+	    	selectedProduct.findElement(By.xpath("(//button[contains(text(),'+')])[" + randomIndex + "]")).click();
+	    	Thread.sleep(2000);
 	    	driver.findElement(By.xpath("(//button[@class='qty-btn varient-btn-plus'])[1]")).click();
 	    	driver.findElement(By.xpath("//*[text()='Done']")).click();
 	    }
 	    else
 	    {
-	    	
+	    	selectedProduct.findElement(By.xpath("(//button[contains(text(),'+')])[" + randomIndex + "]")).click();
 	    }
 	    	
 	    Navigate_HomePage();
@@ -104,13 +107,13 @@ public class HomePage
     {
     	//Home Button
 	    driver.findElement(By.xpath("//img[contains(@class,'fluid desktop-logo')]")).click();
-	    Thread.sleep(2000);
+	    Thread.sleep(1000);
     }
     
     public void Click_on_Cart_Icon() throws InterruptedException
     {
     	//Click on cart Icon
-    	Thread.sleep(2000);
+    	Thread.sleep(1000);
     	driver.findElement(By.xpath("(//li[contains(@class,'cart-btn')])[1]")).click();
     }
         
